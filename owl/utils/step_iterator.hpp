db57d480e7ba3afc29480c86c9fa7fbf1ab50972
Sören König <soerenkoenig@googlemail.com>
2017-10-20 17:19:55 +0200

    minor improvements of step_iterator

--- owl/utils/step_iterator.hpp
+++ owl/utils/step_iterator.hpp
@@ -11,142 +11,144 @@
 
 #include <iterator>
 #include <algorithm>
+#include <type_traits>
 
 namespace owl
 {
   namespace utils
   {
-    template<typename Iterator, std::size_t Step>
+    template <typename Iterator>
+    using is_random_access_iterator = std::conditional_t<std::is_same<typename std::iterator_traits<Iterator>::iterator_category, std::random_access_iterator_tag>::value,
+      std::true_type, std::false_type>;
+    
+    /**
+     An iterator adapter class which modifies the step length of its internal iterator.
+     Step is the number of steps applied to the internal iterator
+     */
+    template<typename Iterator, std::size_t StepSize>
     class step_iterator
-      : public std::iterator<typename std::iterator_traits<Iterator>::iterator_category,
-          typename std::iterator_traits<Iterator>::value_type,
-          typename std::iterator_traits<Iterator>::difference_type,
-          typename std::iterator_traits<Iterator>::pointer,
-          typename std::iterator_traits<Iterator>::reference>
     {
+      static_assert(is_random_access_iterator<Iterator>::value, "step_iterator only supports random access iterators");
+
     public:
-        typedef Iterator iterator_type;
-        typedef typename std::iterator_traits<Iterator>::iterator_category iterator_category;
-        typedef typename std::iterator_traits<Iterator>::value_type value_type;
-        typedef typename std::iterator_traits<Iterator>::difference_type difference_type;
-        typedef typename std::iterator_traits<Iterator>::pointer pointer;
-        typedef typename std::iterator_traits<Iterator>::reference reference;
-        using size_type = std::size_t;
+      using base_iterator_type = Iterator;
+      using iterator_category = typename std::iterator_traits<Iterator>::iterator_category;
+      using value_type = typename std::iterator_traits<Iterator>::value_type;
+      using difference_type = typename std::iterator_traits<Iterator>::difference_type;
+      using pointer = typename std::iterator_traits<Iterator>::pointer;
+      using reference = typename std::iterator_traits<Iterator>::reference;
+      using size_type = std::size_t;
+  
+      constexpr static size_type step_size()
+      {
+        return StepSize;
+      }
     
-        constexpr static size_type step_size()
-        {
-          return Step;
-        }
+      step_iterator() = default;
     
-        step_iterator()
-        {
-        }
-    
-        explicit step_iterator( Iterator it)
-          : _current(it)
-        {
-        
-        }
-    
-        template< typename U, std::size_t S2>
-        step_iterator( const step_iterator<U, S2>& other)
-          : _current(other._current)
-        {
-        }
-    
-        template< typename U, std::size_t S2>
-        step_iterator& operator=(const step_iterator<U, S2>& other)
-        {
-          if(this == &other)
-            return *this;
-          _current = other._current;
+      explicit step_iterator(base_iterator_type it)
+        : _current(it)
+      {
+      }
+  
+      template< typename U, std::size_t Step2>
+      step_iterator( const step_iterator<U, Step2>& other)
+        : _current(other._current)
+      {
+      }
+  
+      template< typename U, std::size_t Step2>
+      step_iterator& operator=(const step_iterator<U, Step2>& other)
+      {
+        if(this == &other)
           return *this;
-        }
+        _current = other._current;
+        return *this;
+      }
     
-        template< typename U, std::size_t S2>
-        step_iterator& operator=(Iterator it)
-        {
-          _current = it;
-          return *this;
-        }
+      template< typename U, std::size_t Step2>
+      step_iterator& operator=(Iterator it)
+      {
+        _current = it;
+        return *this;
+      }
+  
+      Iterator base() const
+      {
+        return _current;
+      }
+  
+      reference operator*() const
+      {
+        return *_current;
+      }
+  
+      pointer operator->() const
+      {
+        std::addressof(operator*());
+      }
+  
+      reference operator[](difference_type n) const
+      {
+        auto it = *this;
+        std::advance(it, n);
+        return *it;
+      }
+  
+      step_iterator& operator++()
+      {
+        std::advance(_current, step_size());
+        return *this;
+      }
     
-        Iterator base() const
-        {
-          return _current;
-        }
+      step_iterator& operator--()
+      {
+        std::advance(_current, -step_size());
+        return *this;
+      }
+  
+      step_iterator operator++(int)
+      {
+        auto tmp = *this;
+        operator++();
+        return tmp;
+      }
+  
+      step_iterator operator--(int)
+      {
+        auto tmp = *this;
+        operator--();
+        return tmp;
+      }
     
-        reference operator*() const
-        {
-          return *_current;
-        }
-    
-        pointer operator->() const
-        {
-          std::addressof(operator*());
-        }
-    
-        reference operator[](difference_type n) const
-        {
-          auto it = *this;
-          std::advance(it, n);
-          return *it;
-        }
-    
-        step_iterator& operator++()
-        {
-          std::advance(_current, step_size());
-          return *this;
-        }
-    
-        step_iterator& operator--()
-        {
-          std::advance(_current, -step_size());
-          return *this;
-        }
-    
-        step_iterator operator++(int)
-        {
-          auto tmp = *this;
-          operator++();
-          return tmp;
-        }
-    
-        step_iterator operator--(int)
-        {
-          auto tmp = *this;
-          operator--();
-          return tmp;
-        }
-    
-        step_iterator& operator+=(difference_type n)
-        {
-          std::advance(_current, step_size()*n);
-          return *this;
-        }
-    
-        step_iterator& operator-=(difference_type n)
-        {
-          std::advance(_current, -step_size()*n);
-          return *this;
-        }
-    
-        step_iterator operator+(difference_type n) const
-        {
-          auto it = *this;
-          it += n;
-          return it;
-        }
-    
-        step_iterator operator-(difference_type n) const
-        {
-          auto it = *this;
-          it -= n;
-          return it;
-        }
+      step_iterator& operator+=(difference_type n)
+      {
+        std::advance(_current, step_size()*n);
+        return *this;
+      }
+  
+      step_iterator& operator-=(difference_type n)
+      {
+        std::advance(_current, -step_size()*n);
+        return *this;
+      }
+  
+      step_iterator operator+(difference_type n) const
+      {
+        auto it = *this;
+        it += n;
+        return it;
+      }
+  
+      step_iterator operator-(difference_type n) const
+      {
+        auto it = *this;
+        it -= n;
+        return it;
+      }
     
     private:
-    
-      Iterator _current;
+      iterator_type _current;
     };
   
     template< typename Iterator, std::size_t Step >
