8003f3f9002bff99d2987f6d0b142f4897baac63
Sören König <soerenkoenig@googlemail.com>
2017-11-01 16:16:43 +0100

    hash utils added uuid implementation continued

--- owl/utils/uuid.hpp
+++ owl/utils/uuid.hpp
@@ -14,6 +14,8 @@
 #include <random>
 #include <limits>
 
+#include "owl/utils/hash_utils.hpp"
+
 namespace owl
 {
   namespace utils
@@ -30,52 +32,26 @@ namespace owl
       using size_type = container_type::size_type;
       using difference_type = container_type::difference_type;
       
-      iterator begin() noexcept { return data.begin(); }
-      const_iterator begin() const noexcept { return data.cbegin(); }
-      iterator end() noexcept { return data.end(); }
-      const_iterator end() const noexcept { return data.cend(); }
+      iterator begin() noexcept;
+      const_iterator begin() const noexcept;
+      iterator end() noexcept;
+      const_iterator end() const noexcept;
       
-      constexpr size_type size() const noexcept { return data.size(); }
+      constexpr size_type size() noexcept;
       
-      bool is_nil() const noexcept
-      {
-        for (std::size_t i = 0; i < sizeof(data); ++i)
-        {
-          if (data[i] != 0U)
-            return false;
-        }
-        return true;
-      }
+      bool is_nil() const noexcept;
       
-      bool operator==(uuid const& other) noexcept
-      {
-        return data == other.data;
-      }
+      bool operator==(uuid const& other) noexcept;
       
-      bool operator!=( uuid const& other) noexcept
-      {
-        return data != other.data;
-      }
+      bool operator!=( uuid const& other) noexcept;
       
-      bool operator<( uuid const& other) noexcept
-      {
-        return data < other.data;
-      }
+      bool operator<( uuid const& other) noexcept;
       
-      bool operator>(uuid const& other) noexcept
-      {
-        return data > other.data;
-      }
+      bool operator>(uuid const& other) noexcept;
       
-      bool operator<=(uuid const& other) noexcept
-      {
-        return data <= other.data;
-      }
+      bool operator<=(uuid const& other) noexcept;
       
-      bool operator>=(uuid const& other) noexcept
-      {
-        return data >= data;
-      }
+      bool operator>=(uuid const& other) noexcept;
       
       enum variant_type
       {
@@ -85,28 +61,7 @@ namespace owl
         variant_future // future definition
       };
       
-      variant_type variant() const noexcept
-      {
-        // variant is stored in octet 7
-        // which is index 8, since indexes count backwards
-        std::uint8_t octet7 = data[8]; // octet 7 is array index 8
-        if ( (octet7 & 0x80) == 0x00 )
-        { // 0b0xxxxxxx
-          return variant_ncs;
-        }
-        else if ( (octet7 & 0xC0) == 0x80 )
-        { // 0b10xxxxxx
-          return variant_rfc_4122;
-        }
-        else if ( (octet7 & 0xE0) == 0xC0 )
-        { // 0b110xxxxx
-          return variant_microsoft;
-        }
-        else
-        {
-          return variant_future;
-        }
-      }
+      variant_type variant() const noexcept;
       
       enum version_type
       {
@@ -118,59 +73,14 @@ namespace owl
         version_name_based_sha1 = 5
       };
       
-      version_type version() const noexcept
-      {
-        // version is stored in octet 9
-        // which is index 6, since indexes count backwards
-        std::uint8_t octet9 = data[6];
-        if ( (octet9 & 0xF0) == 0x10 )
-        {
-          return version_time_based;
-        }
-        else if ( (octet9 & 0xF0) == 0x20 )
-        {
-          return version_dce_security;
-        }
-        else if ( (octet9 & 0xF0) == 0x30 )
-        {
-          return version_name_based_md5;
-        }
-        else if ( (octet9 & 0xF0) == 0x40 )
-        {
-          return version_random_number_based;
-        }
-        else if ( (octet9 & 0xF0) == 0x50 )
-        {
-          return version_name_based_sha1;
-        }
-        else
-        {
-          return version_unknown;
-        }
-      }
+      version_type version() const noexcept;
       
-      union
-      {
-        struct
-        {
-          std::uint32_t data1;
-          std::uint16_t data2;
-          std::uint16_t data3;
-          std::uint8_t  data4[8];
-        };
-        std::array<std::uint8_t,16> data;
-      };
+      std::array<std::uint8_t,16> data;
     };
     
-    inline std::size_t hash_value(uuid const& u) noexcept
+    inline std::size_t hash_value(const uuid& u) noexcept
     {
-      std::size_t seed = 0;
-      for(uuid::const_iterator i=u.begin(), e=u.end(); i != e; ++i)
-      {
-        seed ^= static_cast<std::size_t>(*i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-      }
-      
-      return seed;
+      return hash_value(u.begin(),u.end());
     }
    
     class ios_flags_saver
@@ -315,112 +225,21 @@ namespace owl
           }
         }
         
-        if (is) {
+        if (is)
+        {
           std::copy(data, data+16, u.begin());
         }
       }
       return is;
     }
     
-    namespace detail {
-      inline char to_char(size_t i) {
-        if (i <= 9) {
-          return static_cast<char>('0' + i);
-        } else {
-          return static_cast<char>('a' + (i-10));
-        }
-      }
-      
-      inline wchar_t to_wchar(size_t i) {
-        if (i <= 9) {
-          return static_cast<wchar_t>(L'0' + i);
-        } else {
-          return static_cast<wchar_t>(L'a' + (i-10));
-        }
-      }
-      
-    } // namespace detail
-    
-    inline std::string to_string(uuid const& u)
-    {
-      std::string result;
-      result.reserve(36);
-      
-      std::size_t i=0;
-      for (uuid::const_iterator it_data = u.begin(); it_data!=u.end(); ++it_data, ++i) {
-        const size_t hi = ((*it_data) >> 4) & 0x0F;
-        result += detail::to_char(hi);
-        
-        const size_t lo = (*it_data) & 0x0F;
-        result += detail::to_char(lo);
-        
-        if (i == 3 || i == 5 || i == 7 || i == 9) {
-          result += '-';
-        }
-      }
-      return result;
-    }
-    
-
-    inline std::wstring to_wstring(uuid const& u)
-    {
-      std::wstring result;
-      result.reserve(36);
-      
-      std::size_t i=0;
-      for (uuid::const_iterator it_data = u.begin(); it_data!=u.end(); ++it_data, ++i)
-      {
-        const size_t hi = ((*it_data) >> 4) & 0x0F;
-        result += detail::to_wchar(hi);
-        
-        const size_t lo = (*it_data) & 0x0F;
-        result += detail::to_wchar(lo);
-        
-        if (i == 3 || i == 5 || i == 7 || i == 9) {
-          result += L'-';
-        }
-      }
-      return result;
-    }
-    
-    inline uuid random_uuid()
-    {
-      uuid u;
-      
-      int i=0;
-      using Engine = std::mt19937;
-      using Distribution = std::uniform_int_distribution<unsigned int>;
-    
-     
-      
-        static auto generator = std::bind(Distribution(std::numeric_limits<unsigned int>::min()
-                                       , std::numeric_limits<unsigned int>::max()),
-            Engine((unsigned int)time(NULL)));
-      unsigned long random_value = generator();
-      for (uuid::iterator it=u.begin(); it!=u.end(); ++it, ++i) {
-        if (i==sizeof(unsigned long)) {
-          random_value = generator();
-          i = 0;
-        }
-        
-        // static_cast gets rid of warnings of converting unsigned long to boost::uint8_t
-        *it = static_cast<uuid::value_type>((random_value >> (i*8)) & 0xFF);
-      }
-      
-      // set variant
-      // must be 0b10xxxxxx
-      *(u.begin()+8) &= 0xBF;
-      *(u.begin()+8) |= 0x80;
-      
-      // set version
-      // must be 0b0100xxxx
-      *(u.begin()+6) &= 0x4F; //0b01001111
-      *(u.begin()+6) |= 0x40; //0b01000000
-      
-      return u;
-    }
-    
-
    
+    
+    std::string to_string(uuid const& u);
+    
+    std::wstring to_wstring(uuid const& u);
+   
+    
+    uuid random_uuid();
   }
 }
