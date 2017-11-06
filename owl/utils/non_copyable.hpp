f581bcb66540ca2d88ed5f0ea1037619b914e21a
Sören König <soerenkoenig@googlemail.com>
2017-11-06 20:43:33 +0100

    non_copyable added

---
+++ owl/utils/non_copyable.hpp
@@ -0,0 +1,27 @@
+
+//
+//           .___.
+//           {o,o}
+//          ./)_)
+//      owl --"-"---
+//
+//  Copyright © 2017 Sören König. All rights reserved.
+//
+
+#pragma once
+
+namespace owl
+{
+  namespace utils
+  {
+    struct non_copyable
+    {
+      protected:
+      non_copyable() = default;
+      ~non_copyable() = default;
+
+      non_copyable(const non_copyable&) = delete;
+      non_copyable& operator=(const non_copyable&) = delete;
+    };
+  }
+}
