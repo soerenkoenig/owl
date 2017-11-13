0316f3dd1d78cfac448cc245d3b75b523d18188f
Sören König <soerenkoenig@googlemail.com>
2017-11-13 20:47:34 +0100

    fix in det function

--- test/owl/matrix.cpp
+++ test/owl/matrix.cpp
@@ -11,6 +11,7 @@ namespace test
     using namespace owl::math;
     CHECK(approx(0.01).margin(0.1) == 0.0);
     CHECK(approx(rotateX<float>(0)) == matrix44f::identity());
+    CHECK(approx(det(rotateY<double>(4.9))) == 1);
   
     auto I = matrix44f::identity();
     auto V = vector4f{1,2,3,4};
